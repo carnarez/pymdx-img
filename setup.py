cbe5a5287f756f1ae3b5c7a7fcb400905875b0c4
carnarez <11967125+carnarez@users.noreply.github.com>
2021-10-30 20:09:52 +0200

    Fixed mypy demanding demands

--- setup.py
+++ setup.py
@@ -1,12 +1,13 @@
 """Make `markdown-img` installable (via `pip install git+https://...`)."""
 
-import setuptools
+import setuptools  # type: ignore
 
 setuptools.setup(
     author="carnarez",
     description="Add support for image sizing and centering to Python-Markdown.",
-    install_requires = ["markdown"],
+    install_requires=["markdown"],
     name="markdown-img",
+    package_data={"": ["*.pyi"]},
     py_modules=["markdown_img"],
     url="https://github.com/carnarez/markdown-img",
     version="0.0.1",
